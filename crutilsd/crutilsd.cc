23db66008e2f0fbe5aa0901b20cd4fc1c1c34ac7
Alexander Haase <alexander.haase@rwth-aachen.de>
2014-01-24 16:07:45 +0100

    usage of function daemon() in <unistd.h> instead of own daemonize function

--- crutilsd/crutilsd.cc
+++ crutilsd/crutilsd.cc
@@ -36,57 +36,6 @@
 #include "log.h"
 
 
-/* daemonize ()
- *
- * make this process to a daemon
- */
-bool daemonize (crutilsd_log *log) {
-	/* fork parent */
-	pid_t pid;
-	pid = fork();
-	if (pid < 0) {
-		/* an error occured */
-		log->printf(LOG_ERR, "error while forking process");
-		exit(EXIT_FAILURE);
-	} else {
-		/* kill the parent */
-		if (pid > 0) exit(EXIT_SUCCESS);
-
-		/* create a new session */
-		if (setsid() < 0) {
-			log->printf(LOG_ERR, "error while generate new session in forked process");
-			exit (EXIT_FAILURE);
-		} else {
-			/* ignore SIGHUP */
-			signal(SIGHUP, SIG_IGN);
-
-			/* fork the forked process to prevent zombie-processes */
-			pid = fork();
-			if (pid < 0) {
-				/* an error occured */
-				log->printf(LOG_ERR, "error while second forking process");
-				exit(EXIT_FAILURE);
-			} else {
-				/* kill the parent */
-				if (pid > 0) exit(EXIT_SUCCESS);
-
-				/* change working dir */
-				chdir ("/");
-
-				/* don't use umask of parrent processes */
-				umask (0);
-
-				/* close all file descriptors (STDOUT, STDERR, etc. */
-				for (int i = sysconf (_SC_OPEN_MAX); i > 0; i--) close (i);
-
-				log->printf(LOG_INFO, "daemonized process");
-				return true;
-			}
-		}
-	}
-}
-
-
 /* main loop
  *
  * initialisation and fetching codes by device
@@ -121,7 +70,9 @@ int main (int argc, char **argv) {
 
 
 	/* daemonize, if configured */
-	if (config.get_conf_daemonize()) daemonize(&log);
+	if (config.get_conf_daemonize()) {
+		if (daemon(0, 0) < 0) exit(EXIT_FAILURE);
+	}
 
 	exit(EXIT_SUCCESS);
 
